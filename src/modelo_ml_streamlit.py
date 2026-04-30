a8a8ad836f317752d30662970ea4b9670f770c9f
decaalv <decaalv@gmail.com>
2026-04-29 19:57:12 -0600

    Add new success image and update streamlit model interface

--- src/modelo_ml_streamlit.py
+++ src/modelo_ml_streamlit.py
@@ -44,9 +44,9 @@ def prediccion_o_inferencia(pipeline_de_test, datos_de_test):
     return predicciones, predicciones_sin_escalar, datos_de_test
 
 #Diseno de la Interface
-st.title("Proyecto Modelo ML - Nombre Apellido - DATAPATH")
+st.title("Proyecto Modelo ML - David Castillo - MLOPS15 - Modulo 3 - Sesion 7DATAPATH")
 
-image = Image.open('src/images/datapath-logo.png') #COMPLETAR CON UNA IMAGEN
+image = Image.open('src/images/exito.jpeg') #COMPLETAR CON UNA IMAGEN
 st.image(image, use_container_width=True) #use_column_width esta "deprecated"
 
 st.sidebar.write("Suba el archivo CSV correspondiente para realizar la predicción")
